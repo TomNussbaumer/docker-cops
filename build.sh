1c0e3dd0e653e37a61d462b29ce90d2e5900418a
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-08-06 01:09:10 +0200

    removes testcode before image generation

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 [ -d tmp ] && rm -rf tmp
-mkdir tmp
-cd tmp
-mkdir home
-cd home
+mkdir -p tmp/home && cd tmp/home
 git clone https://github.com/seblucas/cops.git
 cd cops
 sed "s|'./'|'./library/'|" < config_local.php.example > config_local.php
 cd ../..
+mv home/cops/test/BaseWithSomeBooks test-library
+rm -rf home/cops/test
 tar -cvf cops.tar home
 cd ..
 docker build -t docker-cops:latest .
+rm tmp/cops.tar
